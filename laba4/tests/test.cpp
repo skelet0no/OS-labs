440c103356158a1470294f73283f08f318232b7b
skelet0no <syefimov10@gmail.com>
2024-12-06 20:53:17 +0300

    add tests4

--- laba4/tests/test.cpp
+++ laba4/tests/test.cpp
@@ -1,53 +1,37 @@
-// #include <gtest/gtest.h>
-// #include <fstream>
-// #include <iostream>
-// #include <string>
-// #include <unistd.h>
-// #include <sys/wait.h>
-// #include <sstream>
-// #include "../laba3/divider.h"
-//
-// std::string runDivider(const std::string &input) {
-//     std::string tempFileName = "test_divider.txt";
-//     std::ofstream outFile(tempFileName);
-//     outFile << input;
-//     outFile.close();
-//
-//     std::ostringstream buffer;
-//     std::streambuf *originalCout = std::cout.rdbuf(buffer.rdbuf());
-//
-//     Divider(tempFileName);
-//
-//     std::cout.rdbuf(originalCout);
-//
-//     remove(tempFileName.c_str());
-//
-//     return buffer.str();
-// }
-//
-// TEST(Test, ValidDivision) {
-//     std::string input = "100 5 2\n50 4 5\n30 3 0\n80 8 2\n20 2 0";
-//     std::string expectedOutput = "Result: 10\nResult: 2\nError: ZeroDivision\nResult: 5\nError: ZeroDivision\n";
-//
-//     EXPECT_EQ(runDivider(input), expectedOutput);
-// }
-//
-// TEST(Test2, EmptyFile) {
-//     std::string input = "";
-//     std::string expectedOutput = "";
-//
-//     EXPECT_EQ(runDivider(input), expectedOutput);
-// }
-//
-// TEST(Test3, SomeSituations) {
-//     std::string input = "0 0 0\n1 1 1\n1 2 3\n0 10 5";
-//     std::string expectedOutput = "Error: ZeroDivision\nResult: 1\nResult: 0\nResult: 0\n";
-//
-//     EXPECT_EQ(runDivider(input), expectedOutput);
-// }
-//
-// int main(int argc, char **argv) {
-//     ::testing::InitGoogleTest(&argc, argv);
-//     return RUN_ALL_TESTS();
-// }
-#include <iostream>
+#include <gtest/gtest.h>
+#include <random>
+#include "../lib1.h"
+#include "../lib2.h"
+
+constexpr int size = 10;
+
+TEST(BubbleSortTests, Sorting){
+    std::vector vector = {1, 10, 5, 2, 7};
+
+    auto result = BubbleSort(vector);
+    std::sort(vector.begin(), vector.end());
+
+    EXPECT_EQ(result, vector);
+}
+
+TEST(HoarSortTests, Sorting){
+    std::vector vector = {1, 10, 5, 2, 7};
+
+    auto result = HoarSort(vector);
+    std::sort(vector.begin(), vector.end());
+
+    EXPECT_EQ(result, vector);
+}
+
+TEST(PrimeNumbersTests, SimpleCorrectCount){
+    EXPECT_EQ(SimplePrimeNumbers(5, 15), 4);
+}
+
+TEST(PrimeNumbersTests, HardCorrectCount){
+    EXPECT_EQ(HardPrimeNumbers(5, 15), 4);
+}
+
+int main(int argc, char **argv) {
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
